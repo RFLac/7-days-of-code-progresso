0609ec6e5830a0e5f76b0471f2e20bf665d178d4
RFLac <rogerflacerda@gmail.com>
2023-11-08 09:13:06 -0300

    Acrescentei comentários

--- dia1.js
+++ dia1.js
@@ -5,17 +5,24 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
+// Usei um único operador = para comparar o valor
+
 if (numeroUm = stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
+// Usei dois operadores == para comparar o valor e o tipo
+
+
 if (numeroTrinta == stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
+
+// Usei um único operador = para comparar o valor
 
 if (numeroDez == stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
